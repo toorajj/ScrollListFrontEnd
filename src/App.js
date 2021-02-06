9cb1e8edc2d4aaced59ee8a411c07a7e6e488068
TJ <tooraj@hotmail.com>
2021-02-06 15:57:08 -0800

    installed Material UI, added Post, Posts, Form to components, impotred into app.js and added to return

--- src/App.js
+++ src/App.js
@@ -1,11 +1,29 @@
 import React from 'react';
+import { Container, AppBar, Typography, Grow, Grid } from '@material-ui/core';
 
+import items from './images/car.jpg'
+import Posts from './components/Posts/Posts'
+import Form  from './components/Form/Form' 
 
 function App() {
   return (
-    <div className="App">
-     <h1> Test </h1>
-    </div>
+    <Container maxwidth="lg">
+      <AppBar position="static" color= "inherit">
+        <Typography variant="h2" align="center">Items</Typography>
+        <img src={items} alt="items" height="60" />
+      </AppBar>
+      <Grow in>
+        <Grid container justify="space-between" alignItems="stretch" spacing={3}> 
+          <Grid item xs={12} sm={7}>
+            <Posts />
+          </Grid>
+          <Grid item xs={12} sm={4}>
+            <Form />
+          </Grid>
+        </Grid>
+
+      </Grow>
+    </Container>
   );
 }
 
