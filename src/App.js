32a51fe53c00e19f09d62a106007b48e5bfa0405
TJ <tooraj@hotmail.com>
2021-02-07 22:42:52 -0800

    statred data pasing using redux

--- src/App.js
+++ src/App.js
@@ -1,6 +1,8 @@
-import React from 'react';
+import React, { useEffect } from 'react';
 import { Container, AppBar, Typography, Grow, Grid } from '@material-ui/core';
+import { useDispatch } from 'react-redux';
 
+import { getPosts } from './actions/posts'
 import items from './images/car.jpg'
 import Posts from './components/Posts/Posts'
 import Form  from './components/Form/Form' 
@@ -9,6 +11,11 @@ import useStyles from './styles';
 function App() {
 
   const classes = useStyles();
+  const dispatch = useDispatch();
+
+  useEffect(() => {
+    dispatch(getPosts());
+  }, [dispatch]);
 
   return (
     <Container maxwidth="lg">
