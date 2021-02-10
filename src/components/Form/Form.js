1147a0357d2e085a9b38314c49b1d130c483e666
TJ <tooraj@hotmail.com>
2021-02-10 13:22:19 -0800

    imported useEffect into components/Form and used to prepopulate post content

--- src/components/Form/Form.js
+++ src/components/Form/Form.js
@@ -1,4 +1,4 @@
-import React, { useState } from 'react';
+import React, { useState, useEffect } from 'react';
 import { TextField, Button, Typography, Paper } from '@material-ui/core';
 import FileBase from 'react-file-base64';
 import { useDispatch, useSelector} from 'react-redux';
@@ -10,8 +10,12 @@ import { createPost, updatePost } from '../../actions/posts';
 const Form =({ currentId, setCurrentId}) => {
     const classes = useStyles();
     const [ postData, setPostData ] = useState({creator: '', title:'', message:'', tags:'', selectedFile:''});
-    const posts = useSelector((state)=> state.posts);
+    const post = useSelector((state)=> currentId ? state.posts.find((p) => p._id ===currentId) : null);
     const dispatch = useDispatch();
+
+    useEffect(() => {
+        if(post) setPostData(post);
+    }, [post])
 
     const handleSubmit = (e) => {
         e.preventDefault();
