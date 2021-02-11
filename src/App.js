b181893b0fec5595987a0c71d9b28566dfd87db7
TJ <tooraj@hotmail.com>
2021-02-11 13:34:45 -0800

    installed react-router-dom, created Home componenet, and moved logic from app.js, imported Home into app and used wrapped in BrowserRouter, added Swith and Route for home  and auth path

--- src/App.js
+++ src/App.js
@@ -1,36 +1,22 @@
-import React, { useState, useEffect } from 'react';
-import { Container, Grow, Grid } from '@material-ui/core';
-import { useDispatch } from 'react-redux';
-import { getPosts } from './actions/posts'
-import Posts from './components/Posts/Posts'
-import Form  from './components/Form/Form' 
-import useStyles from './styles';
+import React from 'react';
+import { Container } from '@material-ui/core';
+import { BrowserRouter, Switch, Route } from 'react-router-dom';
+
 import Navbar from './components/Navbar/Navbar'
+import Home from './components/Home/Home'
 
 function App() {
-  const [currentId, setCurrentId] = useState(null);
-  const classes = useStyles();
-  const dispatch = useDispatch();
-
-  useEffect(() => {
-    dispatch(getPosts());
-  }, [dispatch]);
 
   return (
-    <Container maxwidth="lg">
-      <Navbar />
-      <Grow in>
-        <Grid className={classes.mainContainer} container justify="space-between" alignItems="stretch" spacing={3}> 
-          <Grid item xs={12} sm={7}>
-            <Posts setCurrentId={setCurrentId} />
-          </Grid>
-          <Grid item xs={12} sm={4}>
-            <Form currentId={currentId} setCurrentId={setCurrentId} />
-          </Grid>
-        </Grid>
-
-      </Grow>
-    </Container>
+    <BrowserRouter>
+      <Container maxwidth="lg">
+        <Navbar />
+        <Switch>
+          <Route path="/" exact component={Home} />
+        </Switch>
+        <Home />
+      </Container>
+    </BrowserRouter>
   );
 }
 
--- src/components/Navbar/Navbar.js
+++ src/components/Navbar/Navbar.js
@@ -1,15 +1,19 @@
 import React from 'react';
 import { AppBar, Typography } from '@material-ui/core'
+import { Link } from 'react-router-dom';
+
 import image from '../../images/scrollList.png'
 import useStyles from './styles'
 
 const Navbar = () => {
     const classes = useStyles();
     return (
+      <div> 
       <AppBar className={ classes.appBar } position="static" color= "inherit">
-        <Typography className={ classes.heading } variant="h3" align="center">Scroll</Typography>
+        <Typography component={Link} to="/" className={ classes.heading } variant="h3" align="center">Scroll</Typography>
         <img className={ classes.image } src={image} alt="items" height="60" />
       </AppBar>
+      </div> 
     )
 }
 
