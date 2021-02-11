8dcc933c35a4ff44cb409c9267711e60ae8f348f
TJ <tooraj@hotmail.com>
2021-02-10 16:12:59 -0800

    added logic for likePost to api request, actions, reducers and components/post

--- src/components/Posts/Post/Post.js
+++ src/components/Posts/Post/Post.js
@@ -6,7 +6,7 @@ import MoreHorizIcon from '@material-ui/icons/MoreHoriz';
 import moment from 'moment';
 import { useDispatch } from 'react-redux';
 import useStyles from './styles';
-import { deletePost } from '../../../actions/posts'
+import { deletePost, likePost } from '../../../actions/posts'
 
 const Post =({ post, setCurrentId }) => {
     const classes = useStyles();
@@ -32,7 +32,7 @@ const Post =({ post, setCurrentId }) => {
            
             </CardContent>
             <CardActions className={ classes.cardActions}>
-                <Button size="small" color="primary" onClick={()=> {console.log(post.likeCount);}}>
+                <Button size="small" color="primary" onClick={()=> dispatch(likePost(post._id))}>
                     <ThumbUpAltIcon fontSize="small" />
                     Like {post.likeCount} 
                 </Button>
