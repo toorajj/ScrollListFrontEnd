a87f2da1c4400b4b546d87a2a5eba757d8220be5
TJ <tooraj@hotmail.com>
2021-02-11 14:26:15 -0800

    adding logic to Auth.js for sign up form

--- src/components/Auth/Auth.js
+++ src/components/Auth/Auth.js
@@ -1,11 +1,39 @@
 import React from 'react'
+import { Avatar, Button, Paper, Grid, Typography, Container, TextField } from '@material-ui/core';
+import LockOutlinedIcon from '@material-ui/icons/LockOutlined';
+
+const handleSubmit = () => {
+
+};
+
+const handleChange = () => {
+
+};
 
 const Auth = () => {
     const state = null;
+    const isSigendUp = true;
     return (
-        <div>
-            AUTH
-        </div>
+        <Container maxwith="xs">
+            <Paper elevation={3}>
+                <Avatar>
+                    <LockOutlinedIcon />
+                </Avatar>
+                <Typography variant="h5">{isSigendUp ? 'Sign Up' : 'Sign in'}</Typography>
+                <form onSubmit={handleSubmit}>
+                    <Grid container spacing={2}>
+                        {
+                            isSigendUp && (
+                                <>
+                                <TextField name="First name" label="First Name" handleChange={handleChange} autoFocus xs={6} />
+                                </>
+                            )
+                        }
+                    </Grid>
+                </form>
+
+            </Paper>
+        </Container>
     )
 }
 
