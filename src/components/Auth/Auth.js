4eceab7decfa4284cbc344d275159de2a3fdab40
TJ <tooraj@hotmail.com>
2021-02-11 17:01:43 -0800

    added a hook for action type 'auth' and payload 'google profile and token if successful

--- src/components/Auth/Auth.js
+++ src/components/Auth/Auth.js
@@ -2,13 +2,14 @@ import React, {useState} from 'react';
 import { Avatar, Button, Paper, Grid, Typography, Container, TextField, Icon } from '@material-ui/core';
 import LockOutlinedIcon from '@material-ui/icons/LockOutlined';
 import { GoogleLogin } from 'react-google-login';
+import { useDispatch } from 'react-redux';
 
 import Input from './Input';
 
 const Auth = () => {
     const [showPassword, setShowPassword] = useState(false);
-    const isSigendUp = false;
-    
+    const [isSignedUp, setIsSignedUp] = useState(false);
+    const dispatch = useDispatch();
     const handleSubmit = () => {
 
     };
@@ -19,8 +20,16 @@ const Auth = () => {
     
     const handleShowPassword = () => setShowPassword((prevShowPasword) => !prevShowPasword);
     
-    const googleSuccess = (res) => {
-        console.log(res);
+    const googleSuccess = async (res) => {
+        const result = res?.profileObj;
+        const token = res?.tokenId;
+
+        try {
+            dispatch({ type: 'AUTH', data: {result, token }});
+            
+        } catch (error) {
+            console.log(error);
+        }
     }
     const googleFailure = () => {
         console.log('Google sign in failed')
@@ -32,10 +41,10 @@ const Auth = () => {
                 <Avatar>
                     <LockOutlinedIcon />
                 </Avatar>
-                <Typography variant="h5">{isSigendUp ? 'Sign Up' : 'Sign in'}</Typography>
+                <Typography variant="h5">{isSignedUp ? 'Sign Up' : 'Sign in'}</Typography>
                 <form onSubmit={handleSubmit}>
                     <Grid container spacing={2}>
-                        { isSigendUp && (
+                        { isSignedUp && (
                             <>
                                 <Input name="First name" label="First Name" handleChange={handleChange} autoFocus half />
                                 <Input name="Last name" label="Last Name" handleChange={handleChange} xs={6} />
@@ -43,10 +52,10 @@ const Auth = () => {
                         )}
                         <Input name="email" label="Email Address" handleChange={handleChange} type="email" />
                         <Input name="password" label="Password" handleChange={handleChange} type={showPassword ? "text" : "password"} handleShowPassword={handleShowPassword} />
-                        { isSigendUp && <Input name="confirmPassword" label="Repeat Passwrod" handleChange={handleChange} type="password" />}
+                        { isSignedUp && <Input name="confirmPassword" label="Repeat Passwrod" handleChange={handleChange} type="password" />}
                     </Grid>
                     <Button type="submit" fullWidth variant="contained" color="primary" >
-                        {isSigendUp ? 'Sign Up' : 'Sign In'}    
+                        {isSignedUp ? 'Sign Up' : 'Sign In'}    
                     </Button>
                     <GoogleLogin
                         clientId="456151989560-5trkj9r78s54i8iaj2k7br3dtcshmp4i.apps.googleusercontent.com"
